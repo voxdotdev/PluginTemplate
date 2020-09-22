aecde945e500a871a611476e0e79bed493c6e9fc
Vox <voxdotdev@gmail.com>
2020-09-22 11:25:43 -0500

    Plugin Template

---
+++ PluginClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Oxide.Plugins
+{
+    [Info("Plugin Name", "Author", "1.0.0"]
+    [Description("Insert Fancy Description here")]
+    class PluginClass : CovalencePlugin
+    {
+     private void Init()
+        {
+            Puts("It's aliveee");
+        }
+
+        // The rest of the code magic
+
+        // TODO (you): Make more epic stuff
+    }
+}
